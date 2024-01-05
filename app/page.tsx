f0d18087e064fec939c0b6ed353c7cb331df3f2c
Jimmy <jhimzmabana@gmail.com>
2024-01-05 14:08:33 +0800

    Versioning untracked files

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export const revalidate = 86400
 export default function Home() {
   return (
     <div className="mx-auto">
-      <MyProfilePic />
+      {/* <MyProfilePic /> */}
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         Hello and Welcome 👋&nbsp;
         <span className="whitespace-nowrap">
-          I'm <span className="font-bold">Dave</span>.
+          I'm <span className="font-bold">JIM</span>.
         </span>
       </p>
       {/* @ts-expect-error Server Component */}
